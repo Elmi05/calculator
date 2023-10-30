8d2ef21cc867f2950c68d70ec88a3e9434796912
Elmi05 <karshe0548@gmail.com>
2023-10-30 16:05:00 +0300

    done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,22 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculator(num1, num2, operator) {
+    if (operator === "+") {
+      return num1 + num2;
+    } else if (operator === "-") {
+      return num1 - num2;
+    } else if (operator === "*") {
+      return num1 * num2;
+    } else if (operator === "/") {
+        return num1 / num2;  
+    } else {
+      return "Invalid operator";
+    }
+  }
+  
+  console.log(calculator(5, 3, "+"));
+  console.log(calculator(5, 3, "-"));
+  console.log(calculator(5, 3, "*"));
+  console.log(calculator(5, 3, "/"));
+  console.log(calculator(5, 3, "%"));
